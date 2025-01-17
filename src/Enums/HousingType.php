4c3349a28d08edc8f3896f23ed5f2f8ebffc311c
jn-devops <devops@joy-nostalg.com>
2025-01-17 18:21:02 +0800

    fix town house enum

--- src/Enums/HousingType.php
+++ src/Enums/HousingType.php
@@ -10,7 +10,7 @@ enum HousingType
     case SINGLE_ATTACHED;
     case SINGLE_DETACHED;
     case QUADRUPLEX;
-    case TOWN_HOUSE;
+    case TOWNHOUSE;
     case TWIN_HOMES;
 
     public function getName(): string
@@ -22,7 +22,7 @@ enum HousingType
             self::SINGLE_ATTACHED => 'Single Attached',
             self::SINGLE_DETACHED => 'Single Detached',
             self::QUADRUPLEX => 'Quadruplex',
-            self::TOWN_HOUSE => 'Town House',
+            self::TOWNHOUSE => 'Townhouse',
             self::TWIN_HOMES => 'Twin Homes'
         };
     }
